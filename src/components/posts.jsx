276c9a2e68bba1fa5a57427bfb7388e3d92e848f
Abd <abdullahzwed@gmail.com>
2021-03-27 12:59:00 +0300

    This commit is after completion of the app development.

---
+++ src/components/posts.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import QueryString from "query-string"; //import queryString is OK too
+
+const Posts = ({ match, location }) => {
+  const result = QueryString.parse(location.search);
+  console.log(result);
+  return (
+    <div>
+      <h1>Posts</h1>
+      Year: {match.params.year}, Month: {match.params.month}
+    </div>
+  );
+};
+
+export default Posts;
